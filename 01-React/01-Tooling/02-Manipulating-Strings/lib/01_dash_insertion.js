82baaa964e1c768b4929e1a6737b7bb4893c3990
reona kasuya <reon.kasuya@gmail.com>
2022-05-09 18:23:42 +0900

    redo for dash insertion pseudo

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,70 +1,18 @@
-// function insertDash(word) {
-
-//   // TODO: implement the method and return word with dashes
-//   return null;
-// }
-
-// module.exports = insertDash;
-
-// Break word into array of alphabets
-// Find the vowels 'aeiou'
-// Place dashes between two consonants
-// Use join method to join the array
-
-
 function insertDash(word) {
-  let characters = Array.from(word);
-  numArray = consonantToIndex(characters);
-  console.log(characters)
-  numArray.forEach(num => characters.splice(num, 0, '-'))
-  console.log(characters)
-  let result = characters.join('');
-  console.log(result);
   // TODO: implement the method and return word with dashes
+  if (word === "") {
+    return "";
+  } else if ( // function )
+  return null;
 }
-
-
-
-function addDashToArray(textArray, numArray) {
-  let result = numArray.forEach(num => textArray.splice(num, 0, '-'))
-  console.log(result);
-}
-
-// Determines whether alphabet is a C or V
-function consonantToIndex(array) {
-  let patternArray = [];
-  array.forEach(alphabet => {
-    if (alphabet.match(/[aeiou]/)) {
-      patternArray.push('v')
-    } else if (alphabet.match(/[bcdfghjklmnpqrstvwxys]/)) {
-      patternArray.push('c');
-    }
-  })
-  //  console.log(patternArray);
-  //   console.log(repetitionDetect(patternArray));
-  let indexArray = repetitionDetect(patternArray);
-  console.log(indexArray);
-  return indexArray;
-
-}
-
-
-// Returns array with index positions
-function repetitionDetect(cvArray) {
-  indexArray = [];
-  for (let i = 0; i < cvArray.length; i++) {
-    if (cvArray[i] === cvArray[i - 1]) {
-      // console.log(i)
-      indexArray.push(i);
-    }
-  }
-  // console.log(indexArray);
-  return indexArray;
-}
-
-
-
-
-// insertDash('hello hello')
 
 module.exports = insertDash;
+
+// If vowel is found add one index and splice a dash to that
+// position. (VC-CVC)
+// For composites like internationalization
+// (VC-CVC-CVCV)
+// If there is no CC repetition then it will compound the collection
+// (in-ter-nationalization)
+// (VC-CVC-CVCVVCVCVCVCVVN)
+// So we need a if conditional to detect no CC repeats
