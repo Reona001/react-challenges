4f8b5da12010809850009479f74a7b0db4cd475a
reona kasuya <reon.kasuya@gmail.com>
2022-05-15 23:33:34 +0900

    code working on external compiler

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/01_dash_insertion.js
@@ -1,36 +1,64 @@
-function insertDash(word) {
-  // TODO: implement the method and return word with dashes
-  let wordArray = Array.from(word);
-  //['h','e','l','l','o']
+const consonantRegEx = /[bcdfghjklmnpqrstvwxys]/;
+const vowelRegEx = /[aeiou]/;
 
-  let vowelIndexArray = [];
-  wordArray.forEach(letter => {
-    if (letter.match(/[aeiou]/)) {
-      // vowelIndexArray.push(letter);
-      let vowelIndex = wordArray.indexOf(`${letter}`);
-      vowelIndexArray.push(vowelIndex);
-      // wordArray.splice(`${vowelIndex}`, 0, '-');
-    }
-  })
-  return vowelIndexArray;
-  // if (word === "") {
-  //   return "";
-  // } else if ( // function )
+function insertDash(word) {
+  let wordArray = wordToArray(word);
+  let patternArray = consonantDetect(wordArray);
+  let indexArray = cvArrayToIndex(patternArray);
+  return spliceDash(word, indexArray)
   return null;
 }
 
+
+
+function wordToArray(word) {
+  let wordArray = Array.from(word);
+  return wordArray;
+}
+
+
+
+function consonantDetect(cvArray) {
+  let cvDetectResult = [];
+  for (let i = 0; i < cvArray.length; i++) {
+    if (cvArray[i].match(consonantRegEx)) {
+      cvDetectResult.push('c');
+    } else if (cvArray[i].match(vowelRegEx)) {
+      cvDetectResult.push('v');
+    }
+  }
+  console.log(cvDetectResult);
+  return cvDetectResult;
+}
+
+
+function cvArrayToIndex(cvArray2) {
+  let arrayLength = 0
+  let repeatIndex = [];
+  for (let i = 0; i < cvArray2.length; i++) {
+    if (cvArray2[i] === cvArray2[i + 1] && cvArray2[i] === 'c') {
+      arrayLength++;
+      repeatIndex.push(i + arrayLength);
+    }
+  }
+  return repeatIndex;
+}
+
+
+
+function spliceDash(word, numArray) {
+  let wordArray = wordToArray(word);
+  console.log(numArray)
+  numArray.forEach(num =>
+    wordArray.splice(num, 0, '-'))
+  wordArrayJoin = wordArray.join('');
+  return wordArrayJoin;
+
+}
+
+// console.log(insertDash('hello'));
+// console.log(insertDash('internationalization'))
+// console.log(insertDash('Le Wagon'))
+// should get 'In-ter-nationalization'
+
 module.exports = insertDash;
-
-// If vowel is found add one index and splice a dash to that
-// position. (VC-CVC)
-// For composites like internationalization
-// (VC-CVC-CVCV)
-// If there is no CC repetition then it will compound the collection
-// (in-ter-nationalization)
-// (VC-CVC-CVCVVCVCVCVCVVN)
-//  (VCCVCCVCVVCVCVCVCVVN)
-//   (VCCVCCVCVVCVCVCVCVVN)
-
-
-// [012345678910]
-// So we need a if conditional to detect no CC repeats
