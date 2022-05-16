530b6586aa61d639b4c28091d895950b57b98937
reona kasuya <reon.kasuya@gmail.com>
2022-05-16 16:01:08 +0900

    lowercase for lorem array complete

--- 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
+++ 01-React/01-Tooling/02-Manipulating-Strings/lib/02_counting_words.js
@@ -1,5 +1,13 @@
 function occurrences(text, word) {
-  return null;
+  // text to array split with empty string ''
+  const splitText = text.split(' ');
+  console.log(splitText); // => ['Lorem','ipsum','dolor','sit']
+  function lowerCase(element) {
+    return element.toLowerCase();
+  }
+  const lowerArray = splitText.map(lowerCase);
+  console.log(lowerArray); // => ['lorem','ipsum','dolor','sit']
+  // return null;
 }
 
 module.exports = occurrences;
