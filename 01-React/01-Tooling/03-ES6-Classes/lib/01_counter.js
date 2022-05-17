df56e1f8de8dc7b94a60210601dc954927b5f8a3
reona kasuya <reon.kasuya@gmail.com>
2022-05-17 09:15:25 +0900

    pseudo code written

--- 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
+++ 01-React/01-Tooling/03-ES6-Classes/lib/01_counter.js
@@ -1,10 +1,16 @@
 class Counter {
   constructor(text) {
+    this.text = text;
     // TODO: build an internal Map of word => occurrences.
+    // 1. Run the text into occurence for all text passed into the function
+    // 2. Create a map of words and their occurrences as an iteger (key value pair)
   }
 
   occurrences(word) {
+    return this.occurence
     // TODO: return the number of occurrences
+    // Query for the above map constructor to get integer
+    // by passing in argument 'word'
   }
 }
 
